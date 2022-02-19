7f41276673524757ff4afde7d454f03951229702
woogy55 <70049167+woogy55@users.noreply.github.com>
2022-02-19 17:35:06 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,8 +1,6 @@
-const images = [
-    "0.jpg", "1.jpg",  "2.jpg"
-];
+const images = ["0.jpg", "1.jpg", "2.jpg"];
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
